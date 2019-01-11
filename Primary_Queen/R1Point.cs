f58d1204b26a3841aba5428cb54c91b4bd1d9be4
Lwandile Ganyile <Luganyile@gmail.com>
2019-01-11 10:56:49 +0200

    Done with R1 all methods work properly.

--- Primary_Queen/R1Point.cs
+++ Primary_Queen/R1Point.cs
@@ -15,62 +15,99 @@ namespace Primary_Queen
 
         public R1Point()
         {
-
+            point = new Point();
         }
 
         public R1Point(float xCoordinate)
         {
-
+            point = new Point(new List<float> { xCoordinate},false);
         }
 
+        public R1Point(Point point)
+        {
+            this.point = point;
+        }
+
+        // Status - Checked , Result - working.
         public void setXCoordinate(float xCoordinate)
         {
-
+            point.setAxisAt(0, xCoordinate);
         }
 
+        public Point Position
+        {
+            get { return point; }
+        }
+
+        // Status - Checked , Result - working.
         public float getXCoordinate()
         {
-            return 1.0f;
+            return point.getAxisAt(0);
         }
 
+        // Status - Checked , Result - working.
         public void increaseXCoordinate(float amount)
         {
-
+            point.increaseAxisAt(0,amount);
         }
 
+        // Status - Checked , Result - working.
         public void decreaseXCoordinate(float amount)
         {
-
+            point.decreaseAxisAt(0, amount);
         }
 
+        // Status - Checked , Result - working.
         public int getDimension()
         {
-            throw new NotImplementedException();
+            return point.Dimension;
         }
 
-        public R1Point translate(int coordinateSystemDirection, float amaunt)
+        // Status - Checked , Result - working.
+        public R1Point translate(int coordinateSystemDirection, float amount)
         {
-            throw new NotImplementedException();
+
+            if (coordinateSystemDirection == 1)
+                return new R1Point(point.getAxisAt(0) - amount);
+
+            return new R1Point(point.getAxisAt(0) + amount);
+           
         }
 
+        // Status - Checked , Result - working.
         public R1Point reflectAroundEqualAxis(List<int> axisIndeces, int numberOfTimes)
         {
-            throw new NotImplementedException();
+
+            return this;
         }
 
+        // Status - Checked , Result - working.
         public R1Point reflectAboutAxis(int axisIndex)
         {
-            throw new NotImplementedException();
+            if (axisIndex == 1)
+                return new R1Point(-point.getAxisAt(0));
+            return this;
         }
 
+        // Status - Checked , Result - working.
         public void display()
         {
-            throw new NotImplementedException();
+            Console.Write("(" + point.getAxisAt(0)+ ") ");
         }
 
+        // Status - Checked , Result - working.
         public int CompareTo(R1Point other)
         {
-            throw new NotImplementedException();
+            int returnValue = -99;
+
+            if (point.getAxisAt(0) < other.Position.getAxisAt(0))
+                returnValue = -1;
+            else if (point.getAxisAt(0) > other.Position.getAxisAt(0))
+                returnValue = 1;
+            else if(point.getAxisAt(0) == other.Position.getAxisAt(0))
+                returnValue = 0;
+
+            return returnValue;
         }
     }
 }
