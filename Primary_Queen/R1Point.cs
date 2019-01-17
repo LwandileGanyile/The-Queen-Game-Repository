77c0d094bf402f3ae598aaec988cbba5fb1d43b6
Lwandile Ganyile <Luganyile@gmail.com>
2019-01-17 10:32:53 +0200

    Final version of R1Point with comments.

--- Primary_Queen/R1Point.cs
+++ Primary_Queen/R1Point.cs
@@ -11,6 +11,8 @@ namespace Primary_Queen
 {
     public class R1Point : RetrievableDimension, ITranslate<R1Point>, IComparable<R1Point>, IDisplay, IReflect<R1Point>, Reflectable<R1Point>
     {
+        /* Used to access methods and attributes by this class because any n 
+        dimensional point isn't allowed to directly access the Point class.*/
         private Point point;
 
         public R1Point()
@@ -39,75 +41,81 @@ namespace Primary_Queen
             get { return point; }
         }
 
-        // Status - Checked , Result - working.
+        // Retrieve x- value.
         public float getXCoordinate()
         {
             return point.getAxisAt(0);
         }
 
-        // Status - Checked , Result - working.
+        // Increasing the current x value by amount.
         public void increaseXCoordinate(float amount)
         {
             point.increaseAxisAt(0,amount);
         }
 
-        // Status - Checked , Result - working.
+        // Decreasing the current x value by amount.
         public void decreaseXCoordinate(float amount)
         {
             point.decreaseAxisAt(0, amount);
         }
 
-        // Status - Checked , Result - working.
+        // The dimension will always be 1.
         public int getDimension()
         {
             return point.Dimension;
         }
 
-        // Status - Checked , Result - working.
+        // Move a point a long a number line.
         public R1Point translate(int coordinateSystemDirection, float amount)
         {
 
             if (coordinateSystemDirection == 1)
                 return new R1Point(point.getAxisAt(0) - amount);
+            else if (coordinateSystemDirection >= 2)
+                return new R1Point(point.getAxisAt(0) + amount);
 
-            return new R1Point(point.getAxisAt(0) + amount);
+            return new R1Point(point.getAxisAt(0));
            
         }
 
-        // Status - Checked , Result - working.
+        // The method is unsupported to this class however it is implemented.
+        // The method returns a copy of this instance.
         public R1Point reflectAroundEqualAxis(List<int> axisIndeces, int numberOfTimes)
         {
 
-            return this;
+            return new R1Point(point.getAxisAt(0));
         }
 
-        // Status - Checked , Result - working.
+        // Negative x of this point.
+        public void negateAtCoornate(int axisAt)
+        {
+            if(axisAt==1)
+                point.negateAtCoornate(axisAt);
+
+        }
+
+        // Return a new R1Point as a result of reflecting this R1Point about the origin.
+        // The axisIndex value equals to 0 means reflecting about the origin.
         public R1Point reflectAboutAxis(int axisIndex)
         {
-            if (axisIndex == 1)
+            if (axisIndex == 0)
                 return new R1Point(-point.getAxisAt(0));
-            return this;
+            return new R1Point(point.getAxisAt(0));
         }
 
-        // Status - Checked , Result - working.
+        // Display a 1 dimensional point in the form (x).
         public void display()
         {
-            Console.Write("(" + point.getAxisAt(0)+ ") ");
+            if (point != null)
+                point.display();
         }
 
-        // Status - Checked , Result - working.
+        /* Compare any two R1Point objects, the returned values is -1, 0, 1, or -99
+           if the returned value is -99 it means the two points are of different dimension.*/
         public int CompareTo(R1Point other)
         {
-            int returnValue = -99;
-
-            if (point.getAxisAt(0) < other.Position.getAxisAt(0))
-                returnValue = -1;
-            else if (point.getAxisAt(0) > other.Position.getAxisAt(0))
-                returnValue = 1;
-            else if(point.getAxisAt(0) == other.Position.getAxisAt(0))
-                returnValue = 0;
-
-            return returnValue;
+          
+            return point.compareTo(other.Position);
         }
     }
 }
