1819cf8af471f8d19bd5fd2a6ae5992855d3a12e
Lwandile Ganyile <Luganyile@gmail.com>
2019-02-01 20:56:40 +0200

    Creating first version of Shooting strategy skeleton.

---
+++ ShootingStrategy/ShootingStrategyNumber.cs
@@ -0,0 +1,34 @@
+﻿namespace ShootingStrategy
+{
+
+    public enum Shoot
+    {
+        FIRST_SHOOT,
+        SECOND_SHOOT,
+        THIRD_SHOOT
+    }
+
+    public enum VectorShootType
+    {
+        CLOCKWISE_SHOOT,
+        COUNTER_CLOCKWISE_SHOOT,
+        INCREMENTAL_SHOOT,
+        DECREMENTAL_SHOOT,
+        NONE
+    }
+
+    public enum FunctionShootType
+    {
+        CLOCKWISE_SHOOT,
+        COUNTER_CLOCKWISE_SHOOT,
+        NONE
+    }
+
+    public enum PlaneShootType
+    {   
+        INCREMENTAL_SHOOT,
+        DECREMENTAL_SHOOT,
+        NONE
+
+    }
+}
---
+++ ShootingStrategy/VectorShootingStrategy.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pieces;
+
+namespace ShootingStrategy
+{
+    public class VectorShootingStrategy
+    {
+        private Plane onPlane;
+        private VectorShootType shootType;
+        private List<int> currentVectors;
+
+
+
+        public Plane OnPlane
+        {
+            get
+            {
+                return onPlane;
+            }
+
+            set
+            {
+
+            }
+        }
+
+        public VectorShootType ShootType
+        {
+            get
+            {
+                return shootType;
+            }
+
+            set
+            {
+
+            }
+        }
+
+        public List<int> CurrentVectors
+        {
+            get
+            {
+                return currentVectors;
+            }
+
+            set
+            {
+
+            }
+        }
+
+        
+
+        public VectorShootingStrategy()
+        {
+
+        }
+
+        public VectorShootingStrategy(Plane onPlane, VectorShootType shootType, List<int> currentVectors)
+        {
+
+        }
+
+        public VectorShootingStrategy(Plane onPlane, VectorShootType shootType, Shoot shootingStrategy)
+        {
+
+        }
+    }
+}
